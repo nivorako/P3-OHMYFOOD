18e4678b9dc063bfe22c9811c8cf0e039ced626b
nivorako <nivo.rakoto@yahoo.fr>
2022-07-14 09:18:22 +0200

    set index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const btn  = document.querySelector('.button')
+btn.addEventListener('click', () => {
+    console.log('alert')
+})
+
+const check = document.querySelector('.click')
+check.addEventListener('click', () => {
+    console.log('alert')
+})
