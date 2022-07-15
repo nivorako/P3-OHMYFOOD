057f60a1716cc444f18536ebefa05bb326bc6e82
nivorako <nivo.rakoto@yahoo.fr>
2022-07-15 23:58:08 +0200

    create style folder, remove h2 from footer

--- index.js
+++
@@ -1,9 +0,0 @@
-const btn  = document.querySelector('.button')
-btn.addEventListener('click', () => {
-    console.log('alert')
-})
-
-const check = document.querySelector('.click')
-check.addEventListener('click', () => {
-    console.log('alert')
-})
